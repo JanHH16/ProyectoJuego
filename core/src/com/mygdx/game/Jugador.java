2f747637e7fe4a32d4a08d96958f6c89ab8b3af9
matiasfuentes1 <101372092+matiasfuentes1@users.noreply.github.com>
2022-10-27 21:14:25 +0100

    Crear Clase Jugador

---
+++ core/src/com/mygdx/game/Jugador.java
@@ -0,0 +1,39 @@
+package com.mygdx.game;
+
+public class Jugador {
+
+    private int vidas;
+    private int puntaje;
+    private int nivel;
+
+    public Jugador(int vidas, int puntaje, int nivel) {
+
+        this.vidas = vidas;
+        this.puntaje = puntaje;
+        this.nivel = nivel;
+    }
+
+    public int getVidas() {
+        return vidas;
+    }
+
+    public int getPuntaje() {
+        return puntaje;
+    }
+
+    public int getNivel() {
+        return nivel;
+    }
+
+    public void setVidas(int vidas) {
+        this.vidas = vidas;
+    }
+
+    public void setPuntaje(int puntaje) {
+        this.puntaje = puntaje;
+    }
+
+    public void setNivel(int nivel) {
+        this.nivel = nivel;
+    }
+}
